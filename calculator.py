c32ac3521a801fe72de64fcc7a43cc546a6b84a2
lisa <lisavetpolo@gmail.com>
2025-01-09 17:09:43 +0200

    Add calc

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+print("Simple Calculator")
+print("Addition (5 + 3):", add(5, 3))
+print("Subtraction (10 - 4):", subtract(10, 4))
